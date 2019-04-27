466aae1e8c82e9a1ea8b4c1dbcdff82b4a4a6e7e
nikolaypuliaiev <nikolay.puliaiev.dev@gmail.com>
2019-04-27 19:08:20 +0300

    Initialized Google Maps. Created project hierarchy

---
+++ recycling/Constants/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  recycling
+//
+//  Created by Nikolay Puliaiev on 4/27/19.
+//  Copyright © 2019 nikolaypuliaev. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    struct API {
+        //MARK:- GoogleMaps
+        static let googleMapsApiKey = "AIzaSyCAb9Aq-D-DwxMMneUFQ4LxBISkI9HJc7s"
+    }
+    
+    struct AppColors {
+        
+    }
+    
+    struct UserDefaults {
+
+    }
+}
