3f5a0611b0eecf542aac18c59826c808bb1654e3
nikolaypuliaiev <nikolay.puliaiev.dev@gmail.com>
2019-04-27 19:34:22 +0300

    Added ViewControllers

---
+++ recycling/Controllers/FavoriteViewController/FavoriteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavoriteViewController.swift
+//  recycling
+//
+//  Created by Nikolay Puliaiev on 4/27/19.
+//  Copyright © 2019 nikolaypuliaev. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
