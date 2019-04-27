72c095edc5c9cd4ca4ed0f5608d6b0a5c7dfb4be
nikolaypuliaiev <nikolay.puliaiev.dev@gmail.com>
2019-04-27 18:18:57 +0300

    Created project. Added .gitignore

---
+++ recycling/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  recycling
+//
+//  Created by Nikolay Puliaiev on 4/27/19.
+//  Copyright © 2019 nikolaypuliaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
